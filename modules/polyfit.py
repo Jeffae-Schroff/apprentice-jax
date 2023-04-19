Fixed outer loop Hessian, added several debug file TODO: Delete debug files

modules/polyfit.py
@@ -80,30 +80,33 @@ class Polyfit:
             self.has_cov = kwargs['covariance']
             self.reg_mode = 'lst_sq'
             self.reg_param = 0
+            self.objective_func = self.lst_sq
             if 'reg_mode' in kwargs.keys():
                 self.reg_mode = kwargs['reg_mode']
                 self.reg_param = kwargs['reg_param']
+            
 
             f = h5py.File(self.input_h5, "r")
             self.X = jnp.array(f['params'][:], dtype=jnp.float64) #num_mc_runs * dim array
             self.Y = jnp.array(f['values'][:], dtype=jnp.float64) #num_bins * num_mc_runs array
             self.Y_err = jnp.array(f['errors'][:], dtype=jnp.float64)
             self.bin_ids = np.array([x.decode() for x in f.get("index")[:]])
+            self.obs_weights = jnp.ones(jnp.shape(self.bin_ids))
+            self.original_dim = len(self.bin_ids)
             self.dim = self.X.shape[1]
             self.num_coeffs = self.numCoeffsPoly(self.dim, self.order) 
 
             #filter out bins with invalid value (>1000) for any mc_run
-            if self.reg_mode=='ridge_w':
-                invalid = jnp.array(jnp.any((abs(self.Y[:,:]) > 1000)|(self.Y_err[:,:] == 0.0), axis = 1).nonzero()[0])
-            else: #Seems redundant but I am keeping this as a placeholder, allowing 0 values here seems to break something in paramtune for very large data sets
-                invalid = jnp.array(jnp.any((abs(self.Y[:,:]) > 1000)|(self.Y_err[:,:] == 0.0), axis = 1).nonzero()[0])
+            invalid = jnp.array(jnp.any((abs(self.Y[:,:]) > 1000), axis = 1).nonzero()[0])
             if len(invalid) > 0:
                 print("Filtered", len(invalid), "of", len(self.bin_ids), "total bins for invalid input")
             self.Y = jnp.delete(self.Y, invalid, axis=0)
             self.Y_err = jnp.delete(self.Y_err, invalid, axis=0)
             self.bin_ids = np.delete(self.bin_ids, invalid)
-            
+            self.obs_weights = jnp.delete(self.obs_weights, invalid)
+
             # If fit_obs is in arugments, it is a list of which observables to use (by order in the input file)
+            #Replaced by select_obs, can delete?
             if 'fit_obs' in kwargs.keys():
                 #Won't truncate e.g. d100, but will add leading zeroes to single digits to match pythia (?) format
                 fit_obs = ['d' + str(obs).zfill(2) + '-' for obs in kwargs['fit_obs']]
@@ -113,17 +116,63 @@ class Polyfit:
                 invalid = jnp.array([i for i, bin in enumerate(self.bin_ids) if not any([obs in bin for obs in fit_obs])])
                 self.Y = jnp.delete(self.Y, invalid, axis=0)
                 self.Y_err = jnp.delete(self.Y_err, invalid, axis=0)
-                self.bin_ids = jnp.delete(self.bin_ids, invalid)
+                self.bin_ids = np.delete(self.bin_ids, invalid)
+                self.obs_weights = jnp.delete(self.obs_weights, invalid)
+
             if 'num_bins' in kwargs.keys():
                 #Won't truncate e.g. d100, but will add leading zeroes to single digits to match pythia (?) format
                 num_bins = kwargs['num_bins']
                 print("Selecting ", num_bins, " bins for fitting.")
 
-                #filter out bins with name that does not match fit_obs
                 if num_bins < self.bin_ids.size: 
                     self.Y = self.Y[0:num_bins]
                     self.Y_err = self.Y_err[0:num_bins]
                     self.bin_ids = self.bin_ids[0:num_bins]
+
+            #Only fit observables from given list
+            if 'select_obs' in kwargs.keys():
+                obs_list = kwargs['select_obs']
+                id_cut = np.array([sb[0] for sb in np.char.rsplit(self.bin_ids, "#", maxsplit=1)])  #Removes bin numbers from index, leaving only a list of observable names
+                inv = np.array([i for i, str in enumerate(id_cut) if not str in obs_list])
+                print("Fitting observables ", obs_list)
+
+                self.Y = jnp.delete(self.Y, inv, axis=0)
+                self.Y_err = jnp.delete(self.Y_err, inv, axis=0)
+                self.bin_ids = np.delete(self.bin_ids, inv)
+                id_cut = np.delete(id_cut, inv)
+                self.obs_weights = jnp.delete(self.obs_weights, inv)
+
+                inv = np.array([])
+                #Selecting bin_num bins from each observable (Only used to reduce number of bins for low-performance testing)
+                if('bin_num' in kwargs.keys()):
+                    obs_bin_num = kwargs['bin_num']
+                    obs_name, counts = np.unique(id_cut, return_counts=True)
+
+                    to_clear = counts - obs_bin_num
+                    clear_dict = dict(zip(obs_name, to_clear))
+
+                    id_cut_rev = np.flip(id_cut)
+
+                    inv = np.array([])
+                    for i, str in enumerate(id_cut_rev):
+                        if clear_dict[str] > 0:
+                            inv = np.append(inv, i)
+                            clear_dict[str] -= 1
+
+                    inv = np.flip(np.array([np.size(id_cut_rev) - 1 - i for i in inv])).astype(int)
+                    self.Y = jnp.delete(self.Y, inv, axis=0)
+                    self.Y_err = jnp.delete(self.Y_err, inv, axis=0)
+                    self.bin_ids = np.delete(self.bin_ids, inv)
+                    id_cut = np.delete(id_cut, inv)
+                    self.obs_weights = jnp.delete(self.obs_weights, inv)
+
+                #Defines weights for each observable, will affect how heavily they are considered in outer-loop optimization
+                if('bin_weights' in kwargs.keys()):
+                    weight_list = kwargs['bin_weights']
+                    for i, weight in enumerate(weight_list):
+                        for j, cut_id in enumerate(id_cut):
+                            if cut_id == obs_list[i]:
+                                self.obs_weights = self.obs_weights.at[j].set(weight)
 
             #sample mc_runs. Number of used bins is consistent (we just filtered bins).
             if not sample is None:                                         #(num MC runs)
@@ -136,6 +185,11 @@ class Polyfit:
                 else:
                     print("invalid sample input")
             
+            #Add small amount to each Y_err (ONLY TO PREVENT ERRORS WHILE WE HAVENT SEPARATED OBSERVABLE ERRORS FROM OBSERVABLES IN THE INPUT DATA)
+            #TODO: DELETE THIS AFTER SEPARATING
+            min_val = jnp.min(self.Y_err[jnp.nonzero(self.Y_err)])
+            self.Y_err_adj = self.Y_err + min_val
+
             # the index keys bin names to the array indexes in f.get(index) with binids matching that bin name
             self.index = {}
             # If bin not a key in index yet, start a new list as its value. Append count to bin's value. 
@@ -152,27 +206,56 @@ class Polyfit:
             self.p_coeffs, self.p_coeffs_err, self.chi2ndf, self.res = [],[],[],[]
             if self.has_cov: self.cov = []
 
-            debug=0 ##TODO: DELETE
+
+            if self.Y.shape[0]/self.original_dim <= 0.005:
+                print("Only attempting to fit ", self.Y.shape[0], " out of ", self.original_dim, " bins.")
+            
+            self.skip_idn = []
+            skip_list = open("skipped_bins.txt", "w")
+            skip_count = 0
             for bin_count, bin_id in enumerate(self.bin_ids):
-                print("\rFitting {:d} of {:d}: {:60s}".format(bin_count + 1, self.Y.shape[0], bin_id), end='')
+                print("\rAttempting to fit {:d} of {:d}: {:60s}".format(bin_count + 1, self.Y.shape[0], bin_id), end='')
                 
                 bin_Y = self.Y[self.bin_idn(bin_id),:]
                 bin_Y_err = self.Y_err[self.bin_idn(bin_id),:]
+                bin_Y_err_adj = self.Y_err_adj[self.bin_idn(bin_id),:]  #TEMPORARY ONLY DELETE AFTER FORMAT CHANGE
+
+                if not jnp.any(bin_Y):
+                    print("\nBin ", bin_count, " identically zero across all runs, skipping!")
+                    skip_list.write(bin_id + "\n")
+                    self.skip_idn.append(bin_count)
+                    skip_count += 1
+                    self.p_coeffs.append(jnp.zeros((VM.shape[1],), dtype=jnp.float32).tolist())
+                    self.res.append(0) #bin_res comes out of lstsq as a list
+                    self.chi2ndf.append(0) #because it's supposed to be /ndf
+                    if self.has_cov:
+                        self.cov.append(jnp.zeros((VM.shape[1],VM.shape[1]), dtype=jnp.float32))
+                    continue
 
                 #polynomialapproximation.coeffsolve2 code
-                obj_args = (bin_Y, bin_Y_err, VM, self.reg_param)
+
                 if self.reg_mode == 'ridge':
-                    guess = jnp.zeros((VM.shape[1],), dtype=jnp.float32)
-                    c_opt = opt.minimize(self.ridge_obj, guess, args=obj_args, method='Nelder-Mead')
-                    bin_p_coeffs = c_opt.x
-                    bin_res = [jnp.sum(jnp.square(bin_Y-VM@bin_p_coeffs))]
+                    self.objective_func = self.ridge_obj
+                    obj_args = (bin_Y, VM, self.reg_param)
                 elif self.reg_mode == 'ridge_w':
-                    guess = jnp.zeros((VM.shape[1],), dtype=jnp.float32)
-                    c_opt = opt.minimize(self.ridge_obj_w, guess, args=obj_args, method='Nelder-Mead')
-                    bin_p_coeffs = c_opt.x
-                    bin_res = [jnp.sum(jnp.square(bin_Y-VM@bin_p_coeffs))]
+                    self.objective_func = self.ridge_obj_w
+                    obj_args = (bin_Y, bin_Y_err_adj, VM, self.reg_param)
+                elif self.reg_mode == 'lasso':
+                    self.objective_func = self.lasso_obj
+                    obj_args = (bin_Y, VM, self.reg_param)
+                elif self.reg_mode == 'lasso_w':
+                    self.objective_func = self.lasso_obj_w
+                    obj_args = (bin_Y, bin_Y_err_adj, VM, self.reg_param)
+                elif self.reg_mode == 'lst_sq_w':
+                    self.objective_func = self.lst_sq_w
+                    obj_args = (bin_Y, bin_Y_err_adj, VM)
                 else:
-                    bin_p_coeffs, bin_res, rank, s  = jnp.linalg.lstsq(VM, bin_Y, rcond=None)
+                    obj_args = (bin_Y, VM)
+
+                guess = jnp.zeros((VM.shape[1],), dtype=jnp.float32)
+                c_opt = opt.minimize(self.objective_func, guess, args=obj_args, method='Nelder-Mead')
+                bin_p_coeffs = c_opt.x
+                bin_res = [jnp.sum(jnp.square(bin_Y-VM@bin_p_coeffs))]
 
                 surrogate_Y = self.surrogate(self.X, bin_p_coeffs)
                 bin_chi2 = jnp.sum(jnp.divide(jnp.power((bin_Y - surrogate_Y), 2), surrogate_Y))
@@ -182,44 +265,21 @@ class Polyfit:
                 self.chi2ndf.append(bin_chi2/(self.num_coeffs-1)) #because it's supposed to be /ndf
                 
                 #Calculating covariance of coefficients using inverse Hessian
-                def mini_res_sq(coeff):
-                    return jnp.sum(jnp.square(bin_Y-VM@coeff))
-                def mini_ridge(coeff):
-                    return self.ridge_obj(coeff, bin_Y, bin_Y_err, VM, self.reg_param)
-                def mini_ridge_w(coeff):
-                    return self.ridge_obj_w(coeff, bin_Y, bin_Y_err, VM, self.reg_param)
+                def mini_obj(coeff):
+                    return self.objective_func(coeff, *obj_args)
                 def Hessian(func):
                     return jax.jacfwd(jax.jacrev(func))
                 #polynomialapproximation.fit code
                 if self.has_cov:
-                    if self.reg_mode == 'ridge':
-                        pcov = jnp.linalg.inv(Hessian(mini_ridge)(bin_p_coeffs))
-                    elif self.reg_mode == 'ridge_w':
-                        pcov = jnp.linalg.inv(Hessian(mini_ridge_w)(bin_p_coeffs))
-                    else:
-                        pcov = jnp.linalg.inv(Hessian(mini_res_sq)(bin_p_coeffs))
+                    pcov = jnp.linalg.inv(Hessian(mini_obj)(bin_p_coeffs))
                     fac = bin_res[0] / (VM.shape[0]-VM.shape[1])
                     self.cov.append(pcov*fac)
 
                     # #Old code!
                     """cov = np.linalg.inv(VM.T@VM)
                     fac = bin_res / (VM.shape[0]-VM.shape[1])"""
-                    if debug==1:
-                        print("\nScaling factor in polyfit: ", fac)
-                        
-    
-                        with jnp.printoptions(precision=3, linewidth=1000, suppress=True, floatmode="fixed"):
-                            print("Y: ", bin_Y)
-                            print("bin_p_coeff: ", bin_p_coeffs)
-                            print("\nVM matrix: \n", VM)
-                            print("\nInverse Hessian of lst_sq: \n ", jnp.linalg.inv(Hessian(mini_res_sq)(bin_p_coeffs)))
-                            print("\nInverse Hessian of ridge: \n ", jnp.linalg.inv(Hessian(mini_ridge)(bin_p_coeffs)))
-                            jnp.save('polyfit_inv_hess_lst_sq', jnp.linalg.inv(Hessian(mini_res_sq)(bin_p_coeffs)))
-                        debug=0
-                    # self.cov.append(cov*fac)
-
-                    #print(bin_id, "\n", bin_p_coeffs, "\n", jnp.sqrt(jnp.diagonal(self.cov[bin_idn])), "\nend")
-                    #print(bin_id, bin_p_coeffs, self.cov[bin_id], " end")
+                
+            print("\n", skip_count, " bins skipped for zeros.")        
             print("\nFits written to", npz_file)
             if npz_file is not None:
                 self.save(npz_file)
@@ -245,15 +305,16 @@ class Polyfit:
             elif self.order != all_dict['order'] or self.dim != all_dict['dim']:
                 print("merging data with different order/dim is not allowed(error)")
             self.num_coeffs = self.numCoeffsPoly(self.dim, self.order)
+            self.skip_idn = all_dict['skip_idn']
 
-            jnp_vars = ['p_coeffs', 'chi2ndf', 'res', 'X', 'Y']
+            jnp_vars = ['p_coeffs', 'chi2ndf', 'res', 'X', 'Y', 'obs_weights']
             if self.has_cov: jnp_vars.append('cov')
             for str in jnp_vars: #jnp: numbers only
                 if new:
                     setattr(self, str, jnp.array(all_dict[str]))
                 else:
                     setattr(self, str, jnp.concatenate([getattr(self, str), all_dict[str]]))
-        
+            
         # We recalculate index, obs_index from bin_ids. I decided to just store bin_ids because 
         # 1. npz likes np lists only and
         # Actually though we might be able to concatenate on merge if we store it. worth thinking about
@@ -272,7 +333,7 @@ class Polyfit:
         all_npz -- filepath for npz file of data
         """
         all_dict = {}
-        all_vars = ['p_coeffs', 'chi2ndf', 'res', 'X', 'Y', 'bin_ids', 'dim', 'order']
+        all_vars = ['p_coeffs', 'chi2ndf', 'res', 'X', 'Y', 'bin_ids', 'obs_weights', 'dim', 'order', 'skip_idn']
         if self.has_cov: all_vars.append('cov') 
         for str in all_vars:
             all_dict[str] = getattr(self, str)
@@ -316,6 +377,8 @@ class Polyfit:
             plt.stairs(jnp.take(ymax, obs_bin_idns), edges, label = 'max')
             plt.legend()
             
+
+    #METHODS RELATING TO SURROGATE FUNCTION
 
     def get_surrogate_func(self, bin_id):
         """
@@ -429,17 +492,35 @@ class Polyfit:
 
 
 
-    #NEW OBJECTIVE FUNCTIONS: RIDGE/LASSO
+    #OBJECTIVE FUNCTIONS
     #coeff: coefficients of polynomial
     #target: y-values given in data
     #VM: terms of polynomial given by vandermonde_jax
     #alpha: ridge parameter
-    def ridge_obj_w(self, coeff, target, target_err, VM, alpha):
+    def lst_sq(self, coeff, target, VM):
+        res_sq = jnp.sum(jnp.square(target - VM@coeff))
+        return res_sq
+    
+    def lst_sq_w(self, coeff, target, target_err, VM):
+        w_res_sq = jnp.sum(jnp.square((target - VM@coeff)/target_err))
+        return w_res_sq
+
+    def ridge_obj_w(self, coeff, target, target_err, VM, alpha):          
         w_res_sq = jnp.sum(jnp.square((target - VM@coeff)/target_err))
         penalty = alpha*(coeff@coeff)
         return w_res_sq + penalty
     
-    def ridge_obj(self, coeff, target, target_err, VM, alpha):
+    def ridge_obj(self, coeff, target, VM, alpha):
         res_sq = jnp.sum(jnp.square(target - VM@coeff))
         penalty = alpha*(coeff@coeff)
+        return res_sq + penalty
+    
+    def lasso_obj_w(self, coeff, target, target_err, VM, alpha):          
+        w_res_sq = jnp.sum(jnp.square((target - VM@coeff)/target_err))
+        penalty = alpha*jnp.sum(jnp.abs(coeff))
+        return w_res_sq + penalty
+    
+    def lasso_obj(self, coeff, target, VM, alpha):
+        res_sq = jnp.sum(jnp.square(target - VM@coeff))
+        penalty = alpha*jnp.sum(jnp.abs(coeff))
         return res_sq + penalty